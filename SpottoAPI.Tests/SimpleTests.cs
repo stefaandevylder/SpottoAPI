8af18779d10fc0cb3ead3c0c372416b1bd2793ef
Stefaan De Vylder <stefaan.devylder@gmail.com>
2023-03-19 19:15:19 +0100

    Finish example test

--- SpottoAPI.Tests/SimpleTests.cs
+++ SpottoAPI.Tests/SimpleTests.cs
@@ -3,6 +3,7 @@ using SpottoAPI.Models.Property;
 using SpottoAPI.Models.Resource;
 using SpottoAPI.Models.Transaction;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -15,18 +16,22 @@ namespace SpottoAPI.Tests {
 
         public SimpleTests(ITestOutputHelper output) {
             _output = output;
-            _client = new SpottoClient("subcriptionKey", "partnerId");
+            _client = new SpottoClient("SubscriptionKey", "PartnerId", true);
         }
 
         [Fact]
-        public void TestUpload() {
+        public async Task TestUploadAsync() {
             SpottoListing listing = new SpottoListing(
                 new SpottoProperty() {
                     Type = PropertyType.Apartment,
+                    SubType = PropertySubType.Penthouse,
+                    PurposeType = PropertyPorposeType.Unknown,
+                    NewBuildType = PropertyNewBuildType.NoNewBuild,
                     Descriptions = new List<Description> {
-                        new Description(DescriptionType.ShortDescription, "nl", "Dit is een test.")
+                        new Description(DescriptionType.Title, "NL", "Dit is een test."),
+                        new Description(DescriptionType.DetailedDescription, "NL", "Dit is een tesdfsdfsdfsdfst."),
                     },
-                    LocationInfo = new LocationInfo {
+                    Location = new LocationInfo {
                         Address = new AddressInfo {
                             Street = "Ten Bos",
                             StreetNumber = "11",
@@ -34,12 +39,13 @@ namespace SpottoAPI.Tests {
                             MunicipalityName = "Sint-Niklaas",
                         }
                     }
-                }, 
+                },
                 new SpottoTransaction() {
-                    Type = TransactionType.Rent,
+                    Type = TransactionType.Sale,
                     AvailabilityStatusType = AvailabilityStatusType.Available,
                     HidePriceDetails = false,
                     ContactInfo = new ContactInfo {
+                        ContactReference = "D&A Vastgoed",
                         ContactPerson = new ContactPerson {
                             Email = "info@dnavastgoed.be",
                             Name = "D&A Vastgoed",
@@ -47,17 +53,16 @@ namespace SpottoAPI.Tests {
                             PictureUrl = "https://dnavastgoed.be/wp-content/uploads/2020/09/Logo-7x7-PNG.png"
                         }
                     },
-                    RentTypeInfo = new RentTypeInfo {
-                        Price = 800,
-                        PricePeriod = RentalPricePeriod.Month,
+                    SaleTypeInfo = new SaleTypeInfo {
+                        Price = 900000
                     }
                 },
                 new SpottoResource() {
                     Images = new List<Image> {
                         new Image {
-                            IsMainImage = true,
-                            DisplayOrder = 1,
-                            FileName = "image.jpg",
+                            DisplayOrder = 0,
+                            FileName = "image",
+                            FileType = ".png",
                             Url = "https://dnavastgoed.be/wp-content/uploads/2020/09/Logo-7x7-PNG.png"
                         }
                     },
@@ -66,8 +71,10 @@ namespace SpottoAPI.Tests {
                     }
                 }
              );
-            _output.WriteLine(listing.ToString());
-            Assert.NotNull(listing);
+
+            var result = await _client.CreatePublication(listing, "TEST1");
+            _output.WriteLine(result.Content);
+            Assert.True(result.IsSuccessful);
         }
 
     }
